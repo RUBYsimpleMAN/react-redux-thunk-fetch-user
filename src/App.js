6c2fcef364f1c5a8e8669a873de9b22dd0eef46a
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-08-17 23:00:47 +0300

    2020-08-17_23-00-47_somePrepare02

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+import Header from '../src/components/header/Header'
+import LeftSide from '../src/components/l-sideBlock/LeftSide'
+import Logo from '../src/components/logo/Logo'
+import RightSide from '../src/components/r-sideBlock/RightSide'
 import UserPage from '../src/components/pages/user/UserPage'
+import Footer from '../src/components/footer/Footer'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-
-    </div>
+    <div className="App"> </div>
   );
 }
 
